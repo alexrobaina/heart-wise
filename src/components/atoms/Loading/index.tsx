dc0be6afca90b83a43132ccad039bd3f054f4aa0
Wayex <30770802+alexrobaina@users.noreply.github.com>
2025-06-12 06:26:47 -0300

    add delete chat (#12)

--- src/components/atoms/Loading/index.tsx
+++ src/components/atoms/Loading/index.tsx
@@ -3,10 +3,13 @@ import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 
 interface Props {
   size?: number
+  centerScreen?: boolean
 }
-export const Loading: FC<Props> = ({ size = 10 }) => {
+export const Loading: FC<Props> = ({ size = 10, centerScreen = true }) => {
   return (
-    <div className="fixed inset-0 flex justify-center items-center">
+    <div
+      className={`${centerScreen && 'fixed inset-0'} flex justify-center items-center`}
+    >
       <AiOutlineLoading3Quarters
         className={`animate-spin h-${size} w-${size} text-amber-500`}
       />
